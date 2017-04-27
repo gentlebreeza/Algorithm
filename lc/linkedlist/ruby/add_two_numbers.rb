d5709b10c3e6655c1cc58ef5d8f768acd5121358
Jason Li <jl5528@nyu.edu>
2017-04-27 00:03:48 -0700

    Adding ruby

---
+++ lc/linkedlist/ruby/add_two_numbers.rb
@@ -0,0 +1,21 @@
+def add_two_numbers(node1, node2)
+    value, carry = 0, 0
+    dummy_node = ListNode.new(0)
+    iterator = dummy_node
+
+    while !!node1 || !!node2 || carry != 0
+        node1_value = node1_value.nil? ? 0 : node1.val
+        node2_value = node2_value.nil? ? 0 : node2.val
+        
+        value = node1_value + node2_value + carry
+        carry = value / 10
+        value = value % 10
+
+        iterator.next = ListNode.new(value)
+        iterator = iterator.next
+        node1 = node1.nil? nil : node1.next
+        noed2 = ndoe2.nil? nil : node2.next
+    end
+
+    dummy_node.next
+end
