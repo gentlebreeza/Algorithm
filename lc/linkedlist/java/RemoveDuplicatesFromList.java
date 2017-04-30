938d6fade019a6fd2a051eb7a6c565de55e2b492
Jason Li <jl5528@nyu.edu>
2017-04-29 23:31:42 -0700

    Problem 'Remove duplicates from linked list'

---
+++ lc/linkedlist/java/RemoveDuplicatesFromList.java
@@ -0,0 +1,36 @@
+package lc.linkedlist.java;
+
+public class RemoveDuplicatesFromList {
+    public ListNode removeDuplicates(ListNode head) {
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+
+        while(head != null && head.next != null) {
+            if(head.val == head.next.val) {
+                head.next = head.next.next;
+            } else {
+                head = head.next;
+            }
+        }
+
+        return dummyHead.next;
+    }
+
+    // more straigh-forward
+    public ListNode removeDuplicatesV2(ListNode head) {
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+
+        while(head != null && head.next != null) {
+            // check head.next != null for some cases liek [1,1,1]
+            // keep removing duplicates
+            while(head.next != null && head.val == head.next.val) {
+                head.next = head.next.next;
+            }
+            // otherwise iterate to next one;
+            head = head.next;
+        }
+
+        return dummyHead.next;
+    }
+}
