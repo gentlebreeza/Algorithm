8f6c1a0368fc241a98e8efe124783cc2c10b01c8
Jason Li <jl5528@nyu.edu>
2017-05-06 21:56:01 -0700

    problem -- merge two sorted linked lists, ruby version

---
+++ lc/linkedlist/ruby/merge_two_sorted_list.rb
@@ -0,0 +1,22 @@
+def merge(head1, head2)
+    dummy_head = ListNode.new 0
+    iterator = dummy_head
+
+    while head1 && head2
+        if head1.val <= head2.val
+            iterator.next = head1
+            head1 = head1.next
+        else
+            iterator.next = head2
+            head2 = head2.next
+        end
+        iterator = iterator.next
+    end
+    if head1
+        iterator.next = head1
+    else
+        iterator.next = head2
+    end
+
+    dummy_head.next
+end
