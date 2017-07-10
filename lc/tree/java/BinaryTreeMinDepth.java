b635c3322baaaedd5aa705d98bee3533cbbfe338
Jason Li <jl5528@nyu.edu>
2017-07-09 23:30:28 -0700

    minimum depth of a binary tree, java version

---
+++ lc/tree/java/BinaryTreeMinDepth.java
@@ -0,0 +1,17 @@
+package lc.tree.java;
+
+public class BinaryTreeMinDepth {
+    public int minDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+
+        if(root.left == null && root.right != null) {
+            return minDepth(root.right) + 1;
+        } else if (root.right == null && root.left != null) {
+            return minDepth(root.left) + 1;
+        }
+
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+}
